eea50f893e6495f351f4c6902c7a38af9690ad3b
Roman Turner <rn.turner@live.com>
2021-06-26 23:37:57 -0700

    🤠

--- twoPointers.js
+++ twoPointers.js
@@ -1,23 +1,23 @@
 let twoSumTwoPointers = (array, target) => {
   // initializing i and j to point at the first and last elements respectively
-  let i = 0;
+  let left = 0;
   let j = array.length - 1;
 
   // Continue looping until our base condition is met
-  while (array[i] + array[j] !== target) {
+  while (array[left] + array[j] !== target) {
     // Increment i (i.e. increase array[i]'s value) if the sum is less than the target
-    if (array[i] + array[j] < target) {
-      i++;
+    if (array[left] + array[j] < target) {
+      left++;
     }
     // Decrement j (i.e. decrease array[j]'s value) if the sum is greater than the target
-    if (array[i] + array[j] > target) {
+    if (array[left] + array[j] > target) {
       j--;
     }
     // Returning false if i >= j (i.e. no solution possible w/given parameters)
-    if (i >= j) return false;
+    if (left >= j) return false;
   }
 
   // This code will be reached only when our base condition is satisfied
-  return [i, j];
+  return [left, j];
 };
 //two pointers moves either pointer based on a conditional
