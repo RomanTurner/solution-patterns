ec8a3ac790899fe07fd94ea1d3d1fb75ac62618b
Roman Turner <rn.turner@live.com>
2021-06-30 23:30:29 -0700

    recursion pratice

--- Recursion/factorialRecursive.js
+++ Recursion/factorialRecursive.js
@@ -85,17 +85,17 @@ console.log(someRecursive([4, 6, 8], (val) => val > 10)); // false
 //spread > concat single interatble
 //https://stackoverflow.com/questions/48865710/spread-operator-vs-array-concat
 
-// function flatten(arr) {
-//   let newArr = [];
-//   for (let i = 0; i < arr.length; i++) {
-//       if (Array.isArray(arr[i])) {
-//           newArr = newArr.concat(flatten(arr[i]));
-//     } else {
-//       newArr.push(arr[i]);
-//     }
-//   }
-//     return newArr
-// }
+function flattenExample(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+          newArr = newArr.concat(flatten(arr[i]));
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+    return newArr
+}
 
 //flat npm package 
 function flat() {
