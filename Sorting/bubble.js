25f118cd82fd2c2cac331c8edbca5cce21d2412c
Roman Turner <rn.turner@live.com>
2021-07-18 13:07:13 -0700

    dsa

--- Sorting/bubble.js
+++ Sorting/bubble.js
@@ -56,21 +56,6 @@ function naiveBubbleSort(arr) {
 let x = [23, 8, 56, 1, 13]
 console.log(naiveBubbleSort(x));
 
-function refactoredBubbleSort(arr) {
-    let noSwaps;
 
-    for (let i = arr.length; i > 0; i--){
-        noSwaps = true;
-        for (let j = 0; j < i - 1; j++){
-             console.log(arr, arr[j], arr[j + 1]);
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1)
-                noSwaps = false;
-            }
-        }
-        if (noSwaps) break;
-    }
-    return arr;
-}
 
 console.log(refactoredBubbleSort(x));
