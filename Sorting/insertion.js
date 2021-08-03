9b46f2900415356a919772d4479debb889b9c2a3
Roman Turner <rn.turner@live.com>
2021-08-02 21:25:04 -0700

    sp

--- Sorting/insertion.js
+++ Sorting/insertion.js
@@ -4,4 +4,21 @@ This is something I need to finish
 git add . 
 git commit -m 'sp'
 git push
-*/ 
+*/
+
+function insertionSort(arr, n) {
+  let i, key, j;
+  for (i = 1; i < n; i++) {
+    key = arr[i];
+    j = i - 1;
+
+    /* Move elements of arr[0..i-1], that are 
+        greater than key, to one position ahead 
+        of their current position */
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+} 
